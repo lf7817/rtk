eaf86b6f1288d99ad1f9ca8798ffb33f748ac497
lf7817 <535536456@qq.com>
2025-09-10 09:45:25 +0800

    build: 注释掉 maven-publish

--- rtk-transport-ble/build.gradle.kts
+++ rtk-transport-ble/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+//    id("maven-publish")
 }
 
 android {
--- rtk-transport/build.gradle.kts
+++ rtk-transport/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("maven-publish")
+//    id("maven-publish")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
